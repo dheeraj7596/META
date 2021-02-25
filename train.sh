58b5be4250ff97c2c92e77d7062101228d70ed5e
dheeraj7596 <dheeraj7596.dm@gmail.com>
2021-02-24 17:06:36 -0800

    Added line replacement code for bash scripts.

--- train.sh
+++ train.sh
@@ -4,6 +4,11 @@ tmp_path=$3
 
 echo "Cloning AutoPhrase.."
 git clone https://github.com/shangjingbo1226/AutoPhrase.git AutoPhrase
+sed -i.bak 's/DEFAULT_TRAIN=${DATA_DIR}\/EN\/DBLP.txt/#DEFAULT_TRAIN=${DATA_DIR}\/EN\/DBLP.txt/' AutoPhrase/auto_phrase.sh
+sed -i.bak 's/RAW_TRAIN=${RAW_TRAIN:- $DEFAULT_TRAIN}/RAW_TRAIN=${DATA_DIR}\/EN\/text.txt/' AutoPhrase/auto_phrase.sh
+rm AutoPhrase/auto_phrase.sh.bak
+sed -i.bak 's/TEXT_TO_SEG=${TEXT_TO_SEG:- ${DATA_DIR}\/EN\/DBLP.5K.txt}/TEXT_TO_SEG=${TEXT_TO_SEG:- ${DATA_DIR}\/EN\/text.txt}/' AutoPhrase/phrasal_segmentation.sh
+rm AutoPhrase/phrasal_segmentation.sh.bak
 echo "Getting phrases.."
 python3 util/write_to_file.py ${dataset_path}
 cd AutoPhrase
@@ -12,4 +17,4 @@ cd AutoPhrase
 cd ..
 cp AutoPhrase/models/DBLP/segmentation.txt ${tmp_path}/segmentation.txt
 python3 util/parse_autophrase_output.py ${tmp_path} ${dataset_path}
-python3 preprocess.py ${dataset_path} ${tmp_path}
+#python3 preprocess.py ${dataset_path} ${tmp_path}
